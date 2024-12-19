5a4231ae6d76d1c1481c4c02a2bdf68d2512b628
LucasDeleplace <Lucasdele59@gmail.com>
2024-12-19 14:10:47 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Calculatrice");
+Console.WriteLine("Entrez un premier nombre :");
+double.TryParse(Console.ReadLine(), out double a);
+Console.WriteLine("Entrez un deuxième nombre :");
+double.TryParse(Console.ReadLine(), out double b);
+Console.WriteLine("Entrez un opérateur (+, -, *, /) :");
+string op = Console.ReadLine();
+double result = 0;
+switch (op)
+{
+    case "+":
+        result = a + b;
+        break;
+    case "-":
+        result = a - b;
+        break;
+    case "*":
+        result = a * b;
+        break;
+    case "/":
+        if (b == 0)
+        {
+            Console.WriteLine("Division par zéro impossible");
+            return;
+        }
+        result = a / b;
+        break;
+    default:
+        Console.WriteLine("Opérateur invalide");
+        return;
+}
+Console.WriteLine("Résultat : " + result);
